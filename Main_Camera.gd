0003fd58391f6640942f086f167b37b61702ff65
drowpotions <charlesgdiaz@gmail.com>
2025-01-17 20:52:27 -0500

    fixed a few bugs in camera script, mock fps sprite implamented

--- Main_Camera.gd
+++ Main_Camera.gd
@@ -1,5 +1,6 @@
 extends Camera3D
 
+@onready var Hit_Label = $"../../../CanvasLayer/Control/Hit_Label"
 var ray_range = 2000
 
 func _input(event):
@@ -7,7 +8,10 @@ func _input(event):
 		get_camera_collision()
 
 func get_camera_collision():
-	var screen_center = get_viewport().get_size() / 2
+	# Get the updated viewport size
+	var viewport_size = get_viewport().get_visible_rect().size
+	var screen_center = viewport_size / 2
+	
 	var ray_origin = project_ray_origin(screen_center)
 	var ray_end = ray_origin + project_ray_normal(screen_center) * ray_range
 	
@@ -18,7 +22,9 @@ func get_camera_collision():
 	var space_state = get_world_3d().direct_space_state
 	var result = space_state.intersect_ray(query)
 	
-	if not result.is_empty():
+	if result and result.has("collider"):
 		print(result.collider.name)
+		Hit_Label.show()
 	else:
 		print("nothing")
+		Hit_Label.hide()
