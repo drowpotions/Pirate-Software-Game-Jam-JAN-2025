88c578ceff3ba931a63e6718c5260b81e215036e
drowpotions <charlesgdiaz@gmail.com>
2025-01-17 20:31:24 -0500

    Skeleton of Raycast for weapon implamented

--- Main_Camera.gd
+++ Main_Camera.gd
@@ -1,11 +1,24 @@
 extends Camera3D
 
+var ray_range = 2000
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _input(event):
+	if Input.is_action_just_pressed("Fire"):
+		get_camera_collision()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func get_camera_collision():
+	var screen_center = get_viewport().get_size() / 2
+	var ray_origin = project_ray_origin(screen_center)
+	var ray_end = ray_origin + project_ray_normal(screen_center) * ray_range
+	
+	var query = PhysicsRayQueryParameters3D.new()
+	query.from = ray_origin
+	query.to = ray_end
+	
+	var space_state = get_world_3d().direct_space_state
+	var result = space_state.intersect_ray(query)
+	
+	if not result.is_empty():
+		print(result.collider.name)
+	else:
+		print("nothing")
