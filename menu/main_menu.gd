a89ff68e1ef9a4236137f044ec49cb3ef5f827c4
drowpotions <charlesgdiaz@gmail.com>
2025-01-19 01:02:13 -0500

    intro added to main menu

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -16,6 +16,8 @@ func _on_charlie_button_pressed() -> void:
 func _on_world_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Test_World/Demo_Scene.tscn")
 
+func _on_intro_pressed() -> void:
+	get_tree().change_scene_to_file("res://VN Segment/Intro.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
