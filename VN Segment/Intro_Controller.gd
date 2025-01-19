05abcd2c7738773023343b96016ed19bd6e79cc1
blazetbd <blazetbd@gmail.com>
2025-01-19 15:21:01 -0500

    Cleanup errors

--- VN Segment/Intro_Controller.gd
+++ VN Segment/Intro_Controller.gd
@@ -5,10 +5,10 @@ extends Camera3D
 func _ready() -> void:
 	pass # Replace with function body.
 
-func _input(InputEvent) -> void:
+func _input(_InputEvent) -> void:
 	if Input.is_action_pressed("esc"):
 		get_tree().change_scene_to_file("res://menu/main_menu.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
