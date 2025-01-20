7526fd2cde6ffc495e50ae067929c1de9282c056
drowpotions <charlesgdiaz@gmail.com>
2025-01-20 12:47:59 -0500

    DOORS DONE

--- Door.gd
+++ Door.gd
@@ -2,8 +2,9 @@ extends Node3D
 @onready var animation_tree: AnimationTree = %AnimationTree
 
 var open_value = -1.0
-var close_value = 1.0
+var close_value = 0.0
 var current_value = 0.0
+var target_value = close_value
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,20 +12,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	current_value = lerp(current_value, target_value, 5 * delta)
 	%AnimationTree.set("parameters/Blend3/blend_amount", current_value)
-	print(current_value)
 	
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
-		if current_value != open_value:
-			current_value = open_value
+		if target_value != open_value:
+			target_value = open_value
 	print("enter")
 	
 	
 func _on_body_exited(body: Node3D) -> void:
 	if body.is_in_group("player"):
-		if current_value != close_value:
-			current_value = close_value
+		if target_value != close_value:
+			target_value = close_value
 	print("exit")
 	
 	
